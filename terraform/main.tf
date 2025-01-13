9d02c2fd5f8b6a2dec0d75124621a8a40f12ade7
skariria <skariria@amazon.fr>
2025-01-13 17:08:49 +0100

    add of sandbox4

--- terraform/main.tf
+++ terraform/main.tf
@@ -71,7 +71,7 @@ module "sandbox4" {
   }
 
   change_management_parameters = {
-    change_requested_by = "HashiCorp Learn good"
+    change_requested_by = "HashiCorp Learn"
     change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
   }
 
