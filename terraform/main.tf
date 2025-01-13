124b1ec8edd3798b4fdf56c52d7fd84d1e379bfb
skariria <skariria@amazon.fr>
2025-01-13 16:52:39 +0100

    add of sandbox4

--- terraform/main.tf
+++ terraform/main.tf
@@ -53,3 +53,31 @@ module "application-spoke" {
 
   account_customizations_name = "application-spoke"
 }
+
+module "sandbox4" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "skariria+equans-lz-aft-sandbox4@amazon.fr"
+    AccountName               = "sandbox4"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "skariria+equans-lz-aft@amazon.fr"
+    SSOUserFirstName          = "ApplicationSpoke"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox4"
+}
